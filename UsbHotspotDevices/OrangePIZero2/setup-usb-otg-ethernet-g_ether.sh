7df3db86240b60e2f11d122409a5d8d55d9248e5
hbaocr <duonghuynhbaocr@gmail.com>
2022-09-27 17:20:04 +0700

    feat: add auto turning on usb0 iface and running post script

--- UsbHotspotDevices/OrangePIZero2/setup-usb-otg-ethernet-g_ether.sh
+++ UsbHotspotDevices/OrangePIZero2/setup-usb-otg-ethernet-g_ether.sh
@@ -59,34 +59,34 @@ allow-hotplug ppp0
 EOF
 
 #'EOF' --> ignore to process $ --> keep $ as it is to print out $para (include $)
-# echo "post-script run after this usb0 plug in at /etc/network/interfaces"
-# sudo bash -c 'cat > /etc/network/if-up.d/iptable-setup.sh' << 'EOF'
-# #!/bin/bash
+echo "post-script run after this usb0 plug in at /etc/network/interfaces"
+sudo bash -c 'cat > /etc/network/if-up.d/iptable-setup.sh' << 'EOF'
+#!/bin/bash
 
-# #this is the accessible internet  iface
-# out_iface=eth0
+#this is the accessible internet  iface
+out_iface=eth0
 
-# #this is the iface to connect to devices(phone)
-# input_iface=usb0
+#this is the iface to connect to devices(phone)
+input_iface=usb0
 
-# config_iptable(){
+config_iptable(){
 
-#     iptables --flush
-#     iptables --table nat --flush
-#     iptables --delete-chain
-#     iptables --table nat --delete-chain
-#     # nat   :   input_iface  --> out_iface ---> Ethernet
-#     iptables --table nat --append POSTROUTING --out-interface $out_iface -j MASQUERADE
-#     iptables --append FORWARD --in-interface $input_iface -j ACCEPT
+    iptables --flush
+    iptables --table nat --flush
+    iptables --delete-chain
+    iptables --table nat --delete-chain
+    # nat   :   input_iface  --> out_iface ---> Ethernet
+    iptables --table nat --append POSTROUTING --out-interface $out_iface -j MASQUERADE
+    iptables --append FORWARD --in-interface $input_iface -j ACCEPT
  
-#     #Thanks to lorenzo
-#     #Uncomment the line below if facing problems while sharing PPPoE, see lorenzo's comment for more details
-#     #iptables -I FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
+    #Thanks to lorenzo
+    #Uncomment the line below if facing problems while sharing PPPoE, see lorenzo's comment for more details
+    #iptables -I FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
  
-#     sysctl -w net.ipv4.ip_forward=1
-#     sudo iptables -t nat -L -n -v
-# }
-# config_iptable
-# EOF
-# sudo chmod +x /etc/network/if-up.d/iptable-setup.sh
+    sysctl -w net.ipv4.ip_forward=1
+    sudo iptables -t nat -L -n -v
+}
+config_iptable
+EOF
+sudo chmod +x /etc/network/if-up.d/iptable-setup.sh
 
