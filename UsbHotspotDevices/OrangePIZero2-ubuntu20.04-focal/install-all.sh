989adccdbfeab392631e39e870061c8a29bf59ea
hbaocr <duonghuynhbaocr@gmail.com>
2022-10-26 14:27:32 +0700

    feat: add focal support

--- UsbHotspotDevices/OrangePIZero2-ubuntu20.04-focal/install-all.sh
+++ UsbHotspotDevices/OrangePIZero2-ubuntu20.04-focal/install-all.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-#orangepi debian buster : Orangepizero2_2.2.0_ubuntu_focal_server_linux4.9.170.7z
+#Orangepizero2_2.2.0_ubuntu_focal_server_linux4.9.170.7z
 #https://drive.google.com/file/d/1LXvrSuMj9fP5Ldnjg1AoVe3AUbsbGWVY/view?usp=sharing
+
+chmod +x *.sh
 
 sudo ./install-facilities.sh
 sudo ./setup-network-iface.sh
