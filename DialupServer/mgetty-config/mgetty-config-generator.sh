aa807e85fd9482da9fc88f57b53b08d3d08785df
Vu Phan <ptvu.it@gmail.com>
2023-03-02 00:52:41 -0800

    fix: improve some config for mgetty

--- DialupServer/mgetty-config/mgetty-config-generator.sh
+++ DialupServer/mgetty-config/mgetty-config-generator.sh
@@ -14,21 +14,26 @@ port ttyACM0
  port-owner root
  port-group dialout
  port-mode 0660
+
+ direct no
+ autobauding no
+ blocking no
  data-only yes
- ignore-carrier no
+ modem-type data
+ ignore-carrier true
  toggle-dtr yes
  toggle-dtr-waittime 500
  rings 1
- speed 38400
+ speed 115200
  modem-check-time 160
  #this line is only for HW modem. For vocal sams server this will let the mgetty on and off
  #--> don't use that for sams vocal
- init-chat "" AT&F OK ATZ OK "ATQ0 V1 E0 S0=0 S2=128 &C1 &D2 +FCLASS=0" OK "AT+MS=V34" OK
+ init-chat "" AT+MS=V34 OK AT+MS=V90 OK AT+MS=V92 OK "ATQ0 V1 E0 X4 S0=0 S2=128 &C1 &D2 &K3 +FCLASS=0" OK
+ answer-chat "" ATA CONNECT \c \r
  login-prompt \V\n\r\R \m\n\r\P \S (\I)\n\r(\Y)\n\r@ login:\040
  login-time 120
  issue-file /etc/mgetty/issue.drkumo
 
-debug 6
 port tntb0
  port-owner root
  port-group dialout
@@ -40,13 +45,13 @@ port tntb0
  rings 2
  speed 115200
  modem-check-time 160
- #this line is only for HW modem. For vocal sams server this will let the mgetty on and off 
+ #this line is only for HW modem. For vocal sams server this will let the mgetty on and off
  #--> don't use that for sams vocal
  ##init-chat "" "ATQ0 V1 E0 S0=0 S2=128 &C1 &D2 +FCLASS=0" OK
- 
+
 
  login-prompt \V\n\r\R \m\n\r\P \S (\I)\n\r(\Y)\n\r@ login:\040
- login-time 120 
+ login-time 120
  issue-file /etc/mgetty/issue.tntb0
 
 port tntb1
@@ -62,7 +67,7 @@ port tntb1
  modem-check-time 160
  #init-chat "" "ATQ0 V1 E0 S0=0 S2=128 &C1 &D2 +FCLASS=0" OK
  login-prompt \V\n\r\R \m\n\r\P \S (\I)\n\r(\Y)\n\r@ login:\040
- login-time 120 
+ login-time 120
  issue-file /etc/mgetty/issue.tntb1
 
 
@@ -79,7 +84,7 @@ port tntb2
  modem-check-time 160
  #init-chat "" "ATQ0 V1 E0 S0=0 S2=128 &C1 &D2 +FCLASS=0" OK
  login-prompt \V\n\r\R \m\n\r\P \S (\I)\n\r(\Y)\n\r@ login:\040
- login-time 120 
+ login-time 120
  issue-file /etc/mgetty/issue.tntb2
 
 port tntb3
@@ -95,7 +100,7 @@ port tntb3
  modem-check-time 160
  #init-chat "" "ATQ0 V1 E0 S0=0 S2=128 &C1 &D2 +FCLASS=0" OK
  login-prompt \V\n\r\R \m\n\r\P \S (\I)\n\r(\Y)\n\r@ login:\040
- login-time 120 
+ login-time 120
  issue-file /etc/mgetty/issue.tntb3
 
 port tntb4
@@ -111,7 +116,7 @@ port tntb4
  modem-check-time 160
  #init-chat "" "ATQ0 V1 E0 S0=0 S2=128 &C1 &D2 +FCLASS=0" OK
  login-prompt \V\n\r\R \m\n\r\P \S (\I)\n\r(\Y)\n\r@ login:\040
- login-time 120 
+ login-time 120
  issue-file /etc/mgetty/issue.tntb4
 
 
@@ -128,7 +133,7 @@ port tntb5
  modem-check-time 160
  #init-chat "" "ATQ0 V1 E0 S0=0 S2=128 &C1 &D2 +FCLASS=0" OK
  login-prompt \V\n\r\R \m\n\r\P \S (\I)\n\r(\Y)\n\r@ login:\040
- login-time 120 
+ login-time 120
  issue-file /etc/mgetty/issue.tntb5
 
 port tntb6
@@ -144,7 +149,7 @@ port tntb6
  modem-check-time 160
  #init-chat "" "ATQ0 V1 E0 S0=0 S2=128 &C1 &D2 +FCLASS=0" OK
  login-prompt \V\n\r\R \m\n\r\P \S (\I)\n\r(\Y)\n\r@ login:\040
- login-time 120 
+ login-time 120
  issue-file /etc/mgetty/issue.tntb6
 
 
@@ -161,7 +166,7 @@ port tntb7
  modem-check-time 160
  #init-chat "" "ATQ0 V1 E0 S0=0 S2=128 &C1 &D2 +FCLASS=0" OK
  login-prompt \V\n\r\R \m\n\r\P \S (\I)\n\r(\Y)\n\r@ login:\040
- login-time 120 
+ login-time 120
  issue-file /etc/mgetty/issue.tntb7
 
 EOF
